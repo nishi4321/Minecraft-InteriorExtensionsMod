1f2f3372898e15b98f8939eb732887a294ac7593
nishi4321 <n.nishikita2@gmail.com>
2021-02-18 13:47:45 +0900

    Update glow glass texture.

--- src/main/java/network/nishi/interiorextension/InteriorExtension.java
+++ src/main/java/network/nishi/interiorextension/InteriorExtension.java
@@ -28,7 +28,7 @@ public class InteriorExtension {
 
     public static final String MODID = "interiorextension";
     public static final String MODNAME = "Interior Extension Mod";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     @Mod.Metadata(MODID)
     public static ModMetadata meta;
