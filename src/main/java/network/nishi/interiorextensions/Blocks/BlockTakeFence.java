de87f30c5acb840f6bf8e943e2133d1f3389bd1a
nishi4321 <n.nishikita2@gmail.com>
2021-02-19 10:45:01 +0900

    Fix bamboo fence model and collision box.

--- src/main/java/network/nishi/interiorextensions/Blocks/BlockTakeFence.java
+++ src/main/java/network/nishi/interiorextensions/Blocks/BlockTakeFence.java
@@ -8,14 +8,20 @@ import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import network.nishi.interiorextensions.InteriorExtension;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class BlockTakeFence extends Block {
     public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
@@ -30,6 +36,33 @@ public class BlockTakeFence extends Block {
         this.setHarvestLevel("axe", 0);
         // Default Facing
         this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+    }
+
+    protected static final AxisAlignedBB AABB_BASE_1 =
+            new AxisAlignedBB(0.4375D, 0.0D, 0.0D, 0.5625D, 1.0D, 1.0D);
+    protected static final AxisAlignedBB AABB_BASE_2 =
+            new AxisAlignedBB(0.0D, 0.0D, 0.4375D, 1.0D, 1.0D, 0.5625D);
+
+    @Override
+    public void addCollisionBoxToList(IBlockState state, World worldIn,
+                                      BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes,
+                                      @Nullable Entity entityIn, boolean p_185477_7_) {
+        int facing_direction = ((EnumFacing) state.getValue(FACING)).getHorizontalIndex();
+        if(facing_direction == 1 || facing_direction == 3) {
+            addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_BASE_1);
+        }else{
+            addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_BASE_2);
+        }
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        int facing_direction = ((EnumFacing) state.getValue(FACING)).getHorizontalIndex();
+        if(facing_direction == 1 || facing_direction == 3) {
+            return AABB_BASE_1;
+        }else{
+            return AABB_BASE_2;
+        }
     }
 
     @Override
--- src/main/java/network/nishi/interiorextensions/InteriorExtension.java
+++ src/main/java/network/nishi/interiorextensions/InteriorExtension.java
@@ -32,7 +32,7 @@ public class InteriorExtension {
 
     public static final String MODID = "interiorextensions";
     public static final String MODNAME = "Interior Extension Mod";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
 
     @Mod.Metadata(MODID)
     public static ModMetadata meta;
