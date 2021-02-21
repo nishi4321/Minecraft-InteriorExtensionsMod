bc9c617b24a373cbc80adbfc15a0662c6c86c4c5
nishi4321 <n.nishikita2@gmail.com>
2021-02-22 02:02:44 +0900

    Release v1.3.0

--- src/main/java/network/nishi/interiorextensions/InteriorExtension.java
+++ src/main/java/network/nishi/interiorextensions/InteriorExtension.java
@@ -38,7 +38,7 @@ public class InteriorExtension {
 
     public static final String MODID = "interiorextensions";
     public static final String MODNAME = "Interior Extension Mod";
-    public static final String VERSION = "1.3.0 beta";
+    public static final String VERSION = "1.3.0";
 
     @Mod.Metadata(MODID)
     public static ModMetadata meta;
